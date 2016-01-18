c9c55e72e074c9323f6f2ddc62e004b24ed3d112
Brage Roalkvam <brage89@gmail.com>
2016-01-18 09:30:44 +0100

    add loop examples

--- src/11 - For Loops/program11_2.c
+++ src/11 - For Loops/program11_2.c
@@ -6,8 +6,6 @@ int main()
 
 	for( a = 0, b = 10; a < 2 && b > 7; a ++, b --)
 		printf("a is now: %d \n", a);
-
+		printf("b is now: %d \n", b);
 	return 0;
 }
-
-
--- src/11 - For Loops/program11_4.c
+++ src/11 - For Loops/program11_4.c
@@ -2,28 +2,17 @@
 
 int main()
 {
-	int num;
-	
-	printf(" Enter a number between 1 and 5: ");
-	scanf("%d", &num);
+	int count;
 
-	switch (num)
-	{
-		case 0: printf("Nothing...\n");
-			break;
-		case 1: printf("Yep, getting there...\n");
-			break;
-		case 2: printf("Not quiet...\n");
-			break;
-		case 3: printf("Well, give it another go...\n");
-			break;
-		case 4: printf("Very close...\n");
-			break;
-		case 5: printf("Finally got the picture...\n");
-			break;
-		default: printf("Too large...duh...\n");
-			break;
-	}
+	for (count = 5; count > 0; count --)
+		printf("Hey there, WebStudent!\n");
+
+	//while (count > 0)
+	//{
+	//	printf("Hey there, WebStudent!\n");
+	//	count --;
+	//}
+
 	return 0;
 }
 
